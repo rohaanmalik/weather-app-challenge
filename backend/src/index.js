ee864c00a289f9c55de5015ba168e17bfb177447
rohaan malik <rohaanmalik7@gmail.com>
2021-10-31 17:16:23 -0400

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const weatherRouter = require("./routes/weather")
-
+const cors = require("cors")
 const app = express()
+
+app.use(
+  cors()
+);
+
 app.get("/", (req, res) => {
     res.send("weather-app")
 })
 
 app.use("/weather", weatherRouter)
-
 
 app.listen(process.env.PORT)
