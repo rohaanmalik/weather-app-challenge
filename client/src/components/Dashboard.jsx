25e155033117d02cd53d925e9b38c840f33aba6e
rohaan malik <rohaanmalik7@gmail.com>
2021-10-31 23:13:29 -0400

    dynamic rendering weather cards

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,39 +1,35 @@
 import { VStack, HStack, Spacer} from "@chakra-ui/react";
 import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from 'react-redux';
 import WeatherCard from "./WeatherCard";
 import SearchBar from "./SearchBar";
-import { getWeatherAsync } from "../redux/slices/weatherSlices";
+import {useDispatch, useSelector} from 'react-redux';
 
 
 export default function Dashboard() {
-  const [zip, setZip] = useState("37934");
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(getWeatherAsync({ zip: "37934" }));
-  }, []);
-
-  
-  //select state from store
   const state = useSelector(state => state);
-  const { weather, loading, error } = state;
-  console.log(state);
-  console.log("HIII")
+
+  const { weather } = state;
+  
+  let weatherMap = new Map(Object.entries(state?.weather));
+
+  let widgets = [];
+  for (let i = 0; i < weatherMap.size; i++) {
+    widgets.push(
+      <>
+        <WeatherCard key={i+1000000} />
+        <Spacer key={i*i+9}/>
+      </>
+    );
+  }
 
   return (
     <VStack>
-      <SearchBar/>
+      <SearchBar />
       <HStack>
-      <WeatherCard/>
-      <Spacer/>
-      <WeatherCard/>
-      <Spacer/>
-      <WeatherCard/>
-      <Spacer/>
-      <WeatherCard/>
-      <Spacer/>
-      <WeatherCard/>
+        {widgets}
       </HStack>
     </VStack>
   );
+  
+
 }
