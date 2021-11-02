6ded37b552fd6817de8b6718d055bce79238ff8e
rohaan malik <rohaanmalik7@gmail.com>
2021-11-02 10:41:25 -0400

    modifying the weather card

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -29,60 +29,71 @@ import {useDispatch, useSelector} from 'react-redux';
 export default function WeatherCard() {
 
   const state = useSelector(state => state);
-  const { weather } = state;
-  const { city } = weather;
-  const {timezone: offset} = city;
-  console.log(offset)
+  const { weather } = state || {};
+  const { city } = weather || {};
+  const { timezone: offset } = city || {};
+  const { daily } = weather || {};
+  // console.log(daily)
   let weatherMap = new Map(Object.entries(state?.weather));
   let accordianItems = [];
-  console.log(
+
     weatherMap
       .forEach((value, key) => {
+        let keyDate = key.replace(/(\d\d)\/(\d\d)\/(\d{4})/, "$3-$2-$1");
         if ((new Date(key) !== "Invalid Date") && !isNaN(new Date(key))) {
           let date;
-          value.forEach((elem) => {
-            date = new Date((elem.dt+offset) * 1000)
-            console.log(date.toDateString())
-          });
-          accordianItems.push(
-            <>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      {date.toDateString()}
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  <Table variant="simple">
-                    <Tbody>
-                      <Tr>
-                        <Td>Temperature</Td>
-                        <Td textAlign="right"></Td>
-                      </Tr>
-                      <Tr>
-                        <Td>Feels like</Td>
-                        <Td textAlign="right">
-                          {/* {data.getCityByName.weather.summary.description} */}
-                        </Td>
-                      </Tr>
-                      <Tr>
-                        <Td>Wind speed (mph)</Td>
-                        <Td textAlign="right">
-                          {/* {data.getCityByName.weather.wind.speed + " "} */}
-                        </Td>
-                      </Tr>
-                    </Tbody>
-                  </Table>
-                </AccordionPanel>
-              </AccordionItem>
-            </>
-          );
+          daily
+            .filter((e) => new Date(keyDate).toDateString() === new Date((e.dt + offset) * 1000).toDateString())
+            .forEach((elem) => {
+              date = new Date((elem.dt + offset) * 1000);
+              console.log(elem)
+              accordianItems.push(
+                <>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box flex="1" textAlign="left">
+                          {date?.toDateString()}
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <Table variant="simple">
+                        <Tbody>
+                          <Tr>
+                            <Td>Temperature</Td>
+                            <Td textAlign="right"> H: {elem.temp.max}</Td>
+                            <Td textAlign="right"> L: {elem.temp.min}</Td>
+                          </Tr>
+
+                          <Tr>
+                            <Td>UV Index</Td>
+                            <Td textAlign="right">
+                              {elem.uvi}
+                            </Td>
+                          </Tr>
+
+                          <Tr>
+                            <Td>Feels like</Td>
+                            <Td textAlign="right">
+                            </Td>
+                          </Tr>
+                          <Tr>
+                            <Td>Wind speed (mph)</Td>
+                            <Td textAlign="right">
+                              {elem.wind_speed + " "}
+                            </Td>
+                          </Tr>
+                        </Tbody>
+                      </Table>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </>
+              );
+            });
         } 
-      })
-  );
+      });
 
   return (
     <Stack>
