d328034f48c1e14ab42183944ed9370b7c838798
rohaan malik <rohaanmalik7@gmail.com>
2021-11-02 14:10:56 -0400

    adding loading functionality

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { VStack, HStack, Spacer} from "@chakra-ui/react";
+import { VStack, HStack, Spacer, Spinner} from "@chakra-ui/react";
 import React, {useEffect, useState} from "react";
 import WeatherCard from "./WeatherCard";
 import SearchBar from "./SearchBar";
@@ -8,18 +8,22 @@ import {useDispatch, useSelector} from 'react-redux';
 export default function Dashboard() {
   const state = useSelector(state => state);
 
-  const { weather } = state;
+  const { weather, loading, error } = state || {};
   
   let weatherMap = new Map(Object.entries(state?.weather));
   // console.log(weatherMap)
+  console.log(state)
 
   return (
     <VStack>
-      <SearchBar />
-      <HStack>
-        <WeatherCard  />
-        <Spacer />
-      </HStack>
+      <SearchBar />{" "}
+      {loading && <Spinner/> }
+      {(weather.length !== 0 && !loading ) && (
+        <HStack>
+          <WeatherCard />
+          <Spacer />
+        </HStack>
+      )}
     </VStack>
   );
   
