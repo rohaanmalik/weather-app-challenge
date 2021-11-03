7c2cc0ecc1767bf5c6de57ac261339d8892c4701
rohaan malik <rohaanmalik7@gmail.com>
2021-11-03 14:33:11 -0400

    adding console log when the port starts

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,4 +13,4 @@ app.get("/", (req, res) => {
 
 app.use("/weather", weatherRouter)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
